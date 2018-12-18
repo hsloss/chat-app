8bda90690bec8536abe77737423de785bc8b6528
hsloss <hannahmaguiresloss@gmail.com>
2018-12-18 10:46:09 -0500

    added websockets library and initiated connection to socket.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 'use strict'
 const express = require('express')
+const socket = require('socket.io')
 
 //App setup
 const app = express()
@@ -7,9 +8,17 @@ require('dotenv').config()
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`)
 })
 
-//Static file
+//Static file setup
 app.use(express.static('public'))
+
+//Socket setup
+
+const io = socket(server)
+
+io.on('connection',function(socket){
+  console.log('made socket connection', socket.id)
+})
